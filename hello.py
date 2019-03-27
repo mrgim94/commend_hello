ef6976747835d329a9619932b6d86df172dbbb26
mrgim94 <48742351+mrgim94@users.noreply.github.com>
2019-03-27 10:56:58 +0900

    hello.py midified on GitHub

--- hello.py
+++ hello.py
@@ -1,17 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-// For command line git tutorial
-// Local repository modification
-=======
-//  For command line git tutorial
-
-//  GitHub Modification
->>>>>>> origin/master
-=======
 // For command line git tutorial
 
-// GitHub Modification
->>>>>>> origin/master
+// GitHub modification
 
 print("Hello world!")
 print("Tell your world")
