71f8f84c97e1b75d328718183c4e1fccc99b6b9b
mrgim94 <mrgim94@naver.com>
2019-03-27 10:58:19 +0900

    hello.py modified on Local repository

--- hello.py
+++ hello.py
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 // For command line git tutorial
 // Local repository modification
-=======
-//  For command line git tutorial
-
-//  GitHub Modification
->>>>>>> origin/master
-=======
-// For command line git tutorial
-
-// GitHub Modification
->>>>>>> origin/master
 
 print("Hello world!")
 print("Tell your world")
